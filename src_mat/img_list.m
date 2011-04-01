f5f4bc99ec338c1e71a061464c7d141f60088652
Arash Rouhani <miffoljud@gmail.com>
2011-04-01 14:11:16 +0200

    Untested img_list

--- src_mat/img_list.m
+++ src_mat/img_list.m
@@ -1,17 +1,17 @@
-function [ output_args ] = img_list( ~ )
+function [ output_args ] = img_list( img_dir )
 %IMG_LIST Collects all images with criteria
 %   Detailed explanation goes here
-
-    img_dir = 'img';
 
     if exist(img_dir, 'dir') ~= 7
         error(['Imagefolder `' img_dir '` not found']);
     end
 
     files = dir(img_dir);
+    
+    res = zeros(size(files)-2);
 
-    for i = 1:size(files)
-        disp(files(i));
+    for i = 3:size(files)
+        res(i-2) = imread(files(i));
     end
 
 end
