9de8a2854858954c3b443fe5140e070b4e2d6737
Arash Rouhani <miffoljud@gmail.com>
2011-04-01 14:42:45 +0200

    Commented img_list and wrote some more code

--- src_mat/img_list.m
+++ src_mat/img_list.m
@@ -1,4 +1,4 @@
-function [ output_args ] = img_list( img_dir )
+function [ res ] = img_list( img_dir )
 %IMG_LIST Collects all images with criteria
 %   Detailed explanation goes here
 
@@ -6,12 +6,20 @@ function [ output_args ] = img_list( img_dir )
         error(['Imagefolder `' img_dir '` not found']);
     end
 
-    files = dir(img_dir);
+    % get files in folder
+    files = dir(img_dir); 
     
-    res = zeros(size(files)-2);
+    % Remove nonreal files . and ..
+    files = files(3:length(files));
+    
+    % We have images with dimensions 1200*1600*3. 3 is for RGB
+    res = zeros(size(files), 1200, 1600, 3);
 
-    for i = 3:size(files)
-        res(i-2) = imread(files(i));
+    for i = 1:size(files)
+        path = [img_dir files(i).name]; % Path to current image
+        M = double(imread(path)); % We want double-images, not int8
+        M = M/255; % Range is [0, 1]
+        res(i,:,:,:) = M; % Assignment must be done like this ...
     end
 
 end
