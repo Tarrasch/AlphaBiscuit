16b9c20985c1647c01a36aab111064eab22e037c
Arash Rouhani <miffoljud@gmail.com>
2011-04-01 15:33:03 +0200

    Implemented desaturate

--- src_mat/desaturate.m
+++ src_mat/desaturate.m
@@ -1,7 +1,13 @@
-function [ output_args ] = desaturate( input_args )
+function [ greyscale_img ] = desaturate( rgb_img )
 %DESATURATE Summary of this function goes here
 %   Detailed explanation goes here
 
+IRed = rgb_img(:,:,1);
+IGreen = rgb_img(:,:,2);
+% IBlue = Im(:,:,3);
+IYell = (IRed + IGreen)./(2*sum(Im,3));
+
+greyscale_img = IYell;
 
 end
 
