945aabfdf370dcf5d9a5efeef49a614eac670865
Arash Rouhani <miffoljud@gmail.com>
2011-04-01 13:54:19 +0200

    Before testing find_all (matlab just crashed)

--- src_mat/img_list.m
+++ src_mat/img_list.m
@@ -1,7 +1,18 @@
-function [ output_args ] = img_list( input_args )
+function [ output_args ] = img_list( ~ )
 %IMG_LIST Collects all images with criteria
 %   Detailed explanation goes here
 
+    img_dir = 'img';
+
+    if exist(img_dir, 'dir') ~= 7
+        error('Folder not found');
+    end
+
+    files = dir(img_dir);
+
+    for i = 1:size(files)
+        disp(files(i));
+    end
 
 end
 
