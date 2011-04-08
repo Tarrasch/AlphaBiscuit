ba891bce7f5acdfbde62f0e4788722115cbe46d7
Arash Rouhani <miffoljud@gmail.com>
2011-04-08 16:57:58 +0200

    Added stuff to .gitignore

--- src_mat/contours.m
+++ src_mat/contours.m
@@ -3,13 +3,12 @@ function [ img_out ] = contours( img_in )
 %   Detailed explanation goes here
 
 img = img_in;
-img = imresize(img, 0.5);
-img = desaturate(img);
-img = my_filter(img); % smoothify
-img = prewitt(img);
-img = img > 0.05;
+img = imresize(img, 0.5); figure(1), imshow(img);
+img = desaturate(img); figure(2), imshow(img);
+img = my_filter(img); figure(3), imshow(img); % smoothify 
+img = prewitt(img); figure(4), imshow(img);
+img = img > 0.05; figure(5), imshow(img);
 
-figure(123), imshow(img);
 img_out = img;
 
 end
