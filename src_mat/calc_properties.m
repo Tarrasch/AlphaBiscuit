7bdfa39a3a95f3873fadbec318e75cd64ad44b10
Arash Rouhani <miffoljud@gmail.com>
2011-04-12 20:30:50 +0200

    Added functionality for MajorAxisLength

--- src_mat/calc_properties.m
+++ src_mat/calc_properties.m
@@ -2,11 +2,13 @@ function [ properties ] = calc_properties( img )
 %CALC_PROPERTIES binary image with one cracker
 %   This function highly uses regionsprops, however all
 % values returned are normalized with respect to area, automatically.
-% Some properties of this function aer not supported in regionprops.
+% Some properties of this function are not supported in regionprops.
 
-s = regionprops(img, 'Area', 'Centroid', 'Extent', 'Perimeter', 'Orientation', 'Solidity');
+s = regionprops(img, 'Area', 'Centroid', 'Extent', 'MajorAxisLength', 'Perimeter', 'Solidity');
+
 
 s.Perimeter= s.Perimeter/sqrt(s.Area);
+s.MajorAxisLength= s.MajorAxisLength/sqrt(s.Area);
 
 properties = s;
 
