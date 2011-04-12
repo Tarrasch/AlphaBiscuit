22ce4a06544a09babd844f48d20a20ef0dfac9b1
Arash Rouhani <miffoljud@gmail.com>
2011-04-12 20:25:42 +0200

    Added very early version of calc_properties

---
+++ src_mat/calc_properties.m
@@ -0,0 +1,14 @@
+function [ properties ] = calc_properties( img )
+%CALC_PROPERTIES binary image with one cracker
+%   This function highly uses regionsprops, however all
+% values returned are normalized with respect to area, automatically.
+% Some properties of this function aer not supported in regionprops.
+
+s = regionprops(img, 'Area', 'Centroid', 'Extent', 'Perimeter', 'Orientation', 'Solidity');
+
+s.Perimeter= s.Perimeter/sqrt(s.Area);
+
+properties = s;
+
+end
+
