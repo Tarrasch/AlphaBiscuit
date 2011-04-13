7831cfa588e2110f9a2c8fd98bb0d58d0a787eac
Arash Rouhani <miffoljud@gmail.com>
2011-04-13 16:49:17 +0200

    Implemented property filter

--- src_mat/stat_filterprops.m
+++ src_mat/stat_filterprops.m
@@ -2,7 +2,7 @@ function [ out_STATS ] = stat_filterprops( STATS )
 %STAT_FILTERPROPS Removes non-measureable fields from the stat
 %   Detailed explanation goes here
 
-
+out_STATS = rmfield(STATS, {'Area', 'Centroid', 'MajorAxisLength', 'Perimeter', 'Image'});
 
 end
 
