1704cd61e4bf9c33a4f631ed625e428b162426db
Arash Rouhani <miffoljud@gmail.com>
2011-04-13 13:41:22 +0200

    Filled in some content for setup fle

--- src_mat/setup.m
+++ src_mat/setup.m
@@ -2,4 +2,6 @@
 % By having this file, I can start working immidietly cause
 % I don't need to put in these commands manually all the time
 
-
+Imgs = img_list('img/a/');
+Im = Imgs{1};
+Im_bin = imtobinary(Im);
