200e4b167aa09e304add7f7d601268b1175ff471
Arash Rouhani <miffoljud@gmail.com>
2011-04-13 13:39:03 +0200

    Created setup file

---
+++ src_mat/setup.m
@@ -0,0 +1,5 @@
+% This file contains things I usually always do at startup
+% By having this file, I can start working immidietly cause
+% I don't need to put in these commands manually all the time
+
+
