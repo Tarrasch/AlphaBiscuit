16d152bd07648bdb807d6a87155b048bcb5ea6b1
Arash Rouhani <miffoljud@gmail.com>
2011-04-13 14:22:31 +0200

    Modified imtobinary to use vector operations, much fastergg Introduced the AN suffix to distinguish neutralized properties

--- src_mat/imtobinary.m
+++ src_mat/imtobinary.m
@@ -3,23 +3,13 @@ function [ img_out ] = imtobinary( img_in )
 %   Detailed explanation goes here
 
 img = img_in;
-img = imresize(img, 0.25);
+img = imresize(img, 0.5);
 
+r = img(:,:,1);
+g = img(:,:,2);
+b = img(:,:,3);
 
-[n, m, ~] = size(img);
-
-img_out = zeros(n, m);
-
-for i = 1:n
-    for j = 1:m
-        mx = squeeze(img(i, j, :));
-        r = mx(1); g = mx(2); b = mx(3);
-        res = b < 0.1;% && g > 50 && g < 100 && r > g  && r < 2*g;
-        img_out(i, j) = res;
-    end
-end
-
-
+img_out = b < 0.1;% && g > 50 && g < 100 && r > g  && r < 2*g;
 
 end
 
