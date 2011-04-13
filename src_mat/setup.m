bed1569794e46ac428a2a75ba215a9a507a1f5a0
Arash Rouhani <miffoljud@gmail.com>
2011-04-13 15:03:52 +0200

    Worked somewhat on setup

--- src_mat/setup.m
+++ src_mat/setup.m
@@ -2,6 +2,17 @@
 % By having this file, I can start working immidietly cause
 % I don't need to put in these commands manually all the time
 
+
+all_files = dir('img/');
+folders = zeros(0);
+
+for f = all_files'
+    disp(f.name)
+    if numel(f) == 1
+        folders(end) = f.name
+    end
+end
+
 Imgs = img_list('img/a/');
 Im = Imgs{1};
 
