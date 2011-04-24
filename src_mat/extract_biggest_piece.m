17d33de5b3c7d862d6eff033f4eb54e9f0ef8628
Arash Rouhani <miffoljud@gmail.com>
2011-04-24 20:39:03 +0200

    Implemented untested ex

---
+++ src_mat/extract_biggest_piece.m
@@ -0,0 +1,20 @@
+function [ s ] = extract_biggest_piece( ss )
+%EXTRACT_BIGGEST_PIECE Summary of this function goes here
+%   Detailed explanation goes here
+
+if numel(ss) == 0
+    error('No part exists at all. Perhaps to hard filtering?');
+end
+
+i = 0;
+n = numel(ss);
+for j = 1:n
+    if ss(j).Area > ss(i).Area
+        i = j;
+    end
+end
+
+s = ss(j);
+
+end
+
