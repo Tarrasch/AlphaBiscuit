19cc27f3b101e126c46a403bba1dab080e0a9bfb
Arash Rouhani <miffoljud@gmail.com>
2011-04-24 23:22:43 +0200

    Fixed some errors in bisc_dist function

--- src_mat/bisc_dist.m
+++ src_mat/bisc_dist.m
@@ -5,10 +5,12 @@ function [ dist ] = bisc_dist( b1, b2)
 b1 = stat_filterprops(b1);
 b2 = stat_filterprops(b2);
 
-props1 = fieldnames(b1);
-props2 = fieldnames(b2);
+f = @(b) fieldnames(b);
+props1 = f(b1);
+props2 = f(b2);
 
-if props1 ~= props2
+f = @(x) char(x);
+if all(f(props1) ~= f(props2))
     error('Biscuits should have same properties type');
 end
 
@@ -16,7 +18,8 @@ props = props1;
 n = numel(props);
 dist = 0;
 
-for p_str = props
+for i = 1:n
+    p_str = props{i};
     a = getfield(b1, p_str);
     b = getfield(b2, p_str);
     sum = a + b; % kind of assuming a,b > 0
