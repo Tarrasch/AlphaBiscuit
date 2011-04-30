c16ccda23c4298801df472bfb712fcfcca512c35
Arash Rouhani <miffoljud@gmail.com>
2011-04-30 23:33:58 +0200

    Improved classifciation analyzer with code reuse by using classify

--- src_mat/analyse_classifier.m
+++ src_mat/analyse_classifier.m
@@ -7,12 +7,8 @@ total = 0;
 
 for c = 'a':'ö'
     n = numel(biscs{c});
-    f = @(i) biscs{c}{i};
     for i = 1:n
-        data_set = biscs;
-        data_set{c}(i) = [];
-        res = classifier(data_set, f(i));
-        if c == res
+        if classify(biscs, c, i, classifier) == c
             hits = hits + 1;
         end
     end
