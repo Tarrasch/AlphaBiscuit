4ef5518fc63f018259b8afed3713f6511f5b3498
Arash Rouhani <miffoljud@gmail.com>
2011-05-04 16:31:17 +0200

    Created skeleton for interpreter

---
+++ src_mat/wording/interpret_biscuits.m
@@ -0,0 +1,10 @@
+function [ text ] = interpret_biscuits( s, classifier )
+%INTERPRET_BISCUITS Sweep through characters and make words
+%   The biscuits are expected to be spread around like words on
+%   a paper, firstly from top to bottom and then left to right.
+
+
+
+
+end
+
