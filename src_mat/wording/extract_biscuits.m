2c8a0d5c75dfb4cd853e05742bc609d63029dd98
Arash Rouhani <miffoljud@gmail.com>
2011-05-04 16:01:47 +0200

    Fixed bug in extract_biscuits

--- src_mat/wording/extract_biscuits.m
+++ src_mat/wording/extract_biscuits.m
@@ -1,9 +1,9 @@
-function [ output_args ] = extract_biscuits( STATS )
+function [ STATS ] = extract_biscuits( STATS )
 %EXTRACT_BISCUITS Given biscs, take out only real bisc islands
 %   Detailed explanation goes here
 
-max_area = max(STATS.Area);
-STATS = STATS(find(STATS.Area > 0.3*max_area));
+max_area = max([STATS.Area]);
+STATS = STATS([STATS.Area] > 0.3*max_area);
 
 end
 
