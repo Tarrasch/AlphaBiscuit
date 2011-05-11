9810677093e656c4c622886e167d0920fe12a771
Arash Rouhani <miffoljud@gmail.com>
2011-05-11 13:59:58 +0200

    Fixed bug in stat_filterprops

--- src_mat/features/stat_filterprops.m
+++ src_mat/features/stat_filterprops.m
@@ -2,11 +2,12 @@ function [ out_STATS ] = stat_filterprops( STATS )
 %STAT_FILTERPROPS Removes non-measureable fields from the stat
 %   Detailed explanation goes here
 
-fields = ANFields();
+afields= ANFields();
+fields_to_remove = setdiff(fieldnames(STATS), afields);
 
-for f = fields
-    if ~isfield(STATS, f)
-        STATS = rmfield(STATS, fields);
+for f = fields_to_remove
+    if isfield(STATS, f)
+        STATS = rmfield(STATS, f);        
     end
 end
     
