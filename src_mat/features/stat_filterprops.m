bad1f2cdd72e0625d1b390794ec62e5f887de2f5
Arash Rouhani <miffoljud@gmail.com>
2011-05-11 13:52:38 +0200

    Reimplemetned stat_filterprops to use ANFields

--- src_mat/features/stat_filterprops.m
+++ src_mat/features/stat_filterprops.m
@@ -2,14 +2,10 @@ function [ out_STATS ] = stat_filterprops( STATS )
 %STAT_FILTERPROPS Removes non-measureable fields from the stat
 %   Detailed explanation goes here
 
-fields = {'Area' ...
-        , 'Centroid' ...
-        , 'MajorAxisLength' ...
-        , 'Perimeter' ...
-        , 'Image'};
+fields = ANFields();
 
 for f = fields
-    if isfield(STATS, f)
+    if ~isfield(STATS, f)
         STATS = rmfield(STATS, fields);
     end
 end
