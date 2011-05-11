c9e26cbd0d4b58509f92109daf0d90c605aca10b
Arash Rouhani <miffoljud@gmail.com>
2011-05-11 15:00:09 +0200

    Done for today, was implementing normalize_features

--- src_mat/features/normalize_features.m
+++ src_mat/features/normalize_features.m
@@ -2,26 +2,34 @@ function [ biscs_out ] = normalize_features( biscs )
 %NORMALIZE_FEATURES Normalizes the feature fields
 %   Detailed explanation goes here
 
-
 flat_biscs = flatten_cells(biscs);
 n = numel(flat_biscs);
 
 for f = ANFields()
+    f = f{1};
     if ~isfield(flat_biscs{1}, f)
         continue;
     end
-    col = zeros(n,1);
+    col = zeros(n);
     for i = 1:n
-        col(i) = flat_biscs{1}.(f);
+        col(i) = flat_biscs{i}.(f);
     end
     m = mean(col);
     stddev = std(col);
-    for bl = biscs
-        for b = bl
-            b.(f) = (b.(f)-m)/stddev;
-        end
-    end
+    
+    in_f  = @(elem) elem.(f) = (elem.(f)-m)/stddev;
+    arr_f = @(arr) arrayfun(in_f, arr);
+    biscs = arrayfun(arr_f, biscs);
+%     for bl = biscs
+%         for b = bl
+%             b.(f)
+%             b.(f) = (b.(f)-m)/stddev;
+%             b.(f)
+%         end
+%     end
 end
+
+biscs_out = biscs;
 
 end
 
