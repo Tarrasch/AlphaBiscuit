10077da92ad6836b4b47223a760eb44f0bb2703b
Arash Rouhani <miffoljud@gmail.com>
2011-05-15 21:53:46 +0200

    Implemented number of holes

--- src_mat/classify/analyze_failures.m
+++ src_mat/classify/analyze_failures.m
@@ -11,8 +11,6 @@ for c = 'a':'ö'
         fbiscs = sort_by_nearest(biscs, bisc);
         
         ix = 1;
-        disp(fbiscs{ix}.char)
-        disp(c)
         while fbiscs{ix}.char ~= c && ix <= numel(fbiscs)
             ix = ix + 1;
         end
@@ -20,11 +18,11 @@ for c = 'a':'ö'
             disp('############### FAILURE ################');
             disp('Failure, other cracker was closer!');
             disp('Orig:')
-            disp(bisc);
+            disp(stat_filterprops(bisc));
             disp(['Best Challenger (' num2str(bisc_dist(bisc, fbiscs{1})) ') :'])
-            disp(fbiscs{1});
+            disp(stat_filterprops(fbiscs{1}));
             disp(['Closest real (' num2str(bisc_dist(bisc, fbiscs{ix})) ') (' num2str(ix) ') :'])
-            disp(fbiscs{ix});
+            disp(stat_filterprops(fbiscs{ix}));
             disp('');
             
             figure(1), imshow(bisc.Image);
---
+++ src_mat/features/number_of_holes.m
@@ -0,0 +1,19 @@
+function [ s ] = number_of_holes( s, threshold )
+%NUMBER_OF_HOLES Given a stats adds num_holes to struct
+
+hole_image = s.FilledImage - s.Image;
+hole_image = bwlabeln(hole_image);
+imshow(hole_image);
+props = regionprops(hole_image, 'Area');
+
+count = 0;
+for i = 1:numel(props)
+    if props(i).Area > s.Area*threshold
+        count = count + 1;
+    end
+end
+
+s.num_holes = count;
+ 
+end
+
