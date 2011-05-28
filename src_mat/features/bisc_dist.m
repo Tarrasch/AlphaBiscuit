b347a82c0baedf92e1258bb543fc9373c90bb0b1
Arash Rouhani <miffoljud@gmail.com>
2011-05-28 15:23:53 +0200

    Speeded up bisc_dist alot

--- src_mat/features/bisc_dist.m
+++ src_mat/features/bisc_dist.m
@@ -2,29 +2,37 @@ function [ dist ] = bisc_dist( b1, b2 )
 %BISC_DIST Measure biscuit distance
 %   Detailed explanation goes here
 
-b1 = stat_filterprops(b1);
-b2 = stat_filterprops(b2);
-
-f = @(b) fieldnames(b);
-props1 = f(b1);
-props2 = f(b2);
-
-f = @(x) char(x);
-if all(f(props1) ~= f(props2))
-    error('Biscuits should have same properties type');
-end
-
-props = props1;
-n = numel(props);
 dist = 0;
-
-for i = 1:n
-    p_str = props{i};
-    a = b1.(p_str);
-    b = b2.(p_str);
+for p = ANFields()
+    p = char(p);
+    a = b1.(p);
+    b = b2.(p);
     dist = dist + (a-b)*(a-b);
 end
-dist = sqrt(dist); % not really neccesary but ok
+
+% b1 = stat_filterprops(b1);
+% b2 = stat_filterprops(b2);
+% 
+% f = @(b) fieldnames(b);
+% props1 = f(b1);
+% props2 = f(b2);
+% 
+% f = @(x) char(x);
+% if all(f(props1) ~= f(props2))
+%     error('Biscuits should have same properties type');
+% end
+% 
+% props = props1;
+% n = numel(props);
+% dist = 0;
+% 
+% for i = 1:n
+%     p_str = props{i};
+%     a = b1.(p_str);
+%     b = b2.(p_str);
+%     dist = dist + (a-b)*(a-b);
+% end
+% dist = sqrt(dist); % not really neccesary but ok
 
 end
 
