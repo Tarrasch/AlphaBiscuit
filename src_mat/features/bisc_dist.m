a0e70f66915ced2c84e44573646e9b47c2053b05
Arash Rouhani <miffoljud@gmail.com>
2011-05-28 15:25:37 +0200

    removed comments

--- src_mat/features/bisc_dist.m
+++ src_mat/features/bisc_dist.m
@@ -10,29 +10,5 @@ for p = ANFields()
     dist = dist + (a-b)*(a-b);
 end
 
-% b1 = stat_filterprops(b1);
-% b2 = stat_filterprops(b2);
-% 
-% f = @(b) fieldnames(b);
-% props1 = f(b1);
-% props2 = f(b2);
-% 
-% f = @(x) char(x);
-% if all(f(props1) ~= f(props2))
-%     error('Biscuits should have same properties type');
-% end
-% 
-% props = props1;
-% n = numel(props);
-% dist = 0;
-% 
-% for i = 1:n
-%     p_str = props{i};
-%     a = b1.(p_str);
-%     b = b2.(p_str);
-%     dist = dist + (a-b)*(a-b);
-% end
-% dist = sqrt(dist); % not really neccesary but ok
-
 end
 
