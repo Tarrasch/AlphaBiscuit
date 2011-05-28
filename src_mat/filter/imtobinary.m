9a4f397d6e40a884943ec31136a46c72b0763143
Arash Rouhani <miffoljud@gmail.com>
2011-05-28 12:49:52 +0200

    Speeded up it with reasonable qquality

--- src_mat/filter/imtobinary.m
+++ src_mat/filter/imtobinary.m
@@ -2,31 +2,18 @@ function [ img ] = imtobinary( img )
 %IMTOBINARY Turn an RGB image with biscuits to binary image
 %   Detailed explanation goes here
 
-img = rgb2hsv(img);
-
-% First extract colors channels
-f = @(img) img;
-if max(img) > 1
-    f = @(img) double(img)/255;
-end
-h = f(img(:,:,1));
-s = f(img(:,:,2));
-v = f(img(:,:,3));
-
 % Apply Gaussian filter
-w = fspecial('gaussian', 10, 2);
+w = fspecial('gaussian', 5, 1);
 
 f = @(img) imfilter(img, w, 'replicate');
-h = f(h);
-s = f(s);
-v = f(v);
-
-h = h.*360;
+r = f(img(:,:,1));
+g = f(img(:,:,2));
+b = f(img(:,:,3));
 
 %img_out = s > 0.5 ...
 %        & h > 15 & h < 55 ...
 %        & v > 0.20 ;
-img = h > 15 & h < 55;
+img = r > 50 & g > 30 & g < 200 & b < 150 & r > 1.2*g & g > 1.2*b;
 
 end
 
--- src_mat/io/load_biscs.m
+++ src_mat/io/load_biscs.m
@@ -5,9 +5,8 @@ function [ biscs ] = load_biscs( range )
 
 biscs = cell(255,1); % 'a' ..'åäö' Should fit
 
-f = @(img) img;
-g = @(img) imtobinary(f(img));
-h = @(img) imtobinary(f(img));
+g = @(img) imtobinary(img);
+h = @(img) calc_properties(g(img));
 
 for c = range
     foldr = ['img/' c '/'];
