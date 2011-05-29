d24dc4b19333b537f34556651b2834db40ae120c
Arash Rouhani <miffoljud@gmail.com>
2011-05-29 14:39:36 +0200

    Implemented backward elimination

--- src_mat/classify/backward_elimination.m
+++ src_mat/classify/backward_elimination.m
@@ -3,6 +3,24 @@ function [ features ] = backward_elimination( biscs, classifier, features )
 %   Given a data-set, a classifier and a start set of features,
 %   calculate the best reduced feature set by backward elimination.
 
+n = numel(features);
+best_features = features;
+best_cost = analyse_classifier(biscs, classifier, features);
+
+for i = 1:n
+    red_features = features;
+    red_features(i) = [];
+    cost = analyse_classifier(biscs, classifier, red_features);
+    
+    if cost >= best_cost
+        best_features = red_features;
+        best_cost = cost;
+    end
+end
+
+if numel(best_features) ~= n % if we did eliminate with advantage
+    features = backward_elimination( biscs, classifier, best_features);
+end
 
 end
 
