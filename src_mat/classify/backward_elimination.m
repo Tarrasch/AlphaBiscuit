52a75a6495866db2ef39634a0a2d9068c0d442d4
Arash Rouhani <miffoljud@gmail.com>
2011-05-29 14:30:59 +0200

    Added header for backwards elimination

---
+++ src_mat/classify/backward_elimination.m
@@ -0,0 +1,8 @@
+function [ features ] = backward_elimination( biscs, classifier, features )
+%BACKWARD_ELIMINATION Backward eliminate features away
+%   Given a data-set, a classifier and a start set of features,
+%   calculate the best reduced feature set by backward elimination.
+
+
+end
+
